45075ca96bd41f1724768b3dd53e06ac42742bfd
Dan <danbeladev@gmail.com>
2020-02-16 17:59:42 +0200

    logout button option

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -1,12 +1,25 @@
 package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
 
+    val fbAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+
+        logoutBtn.setOnClickListener {
+            fbAuth.signOut()
+            val signInPage = Intent(this,LoginActivity::class.java)
+            startActivity(signInPage)
+        }
     }
 }
